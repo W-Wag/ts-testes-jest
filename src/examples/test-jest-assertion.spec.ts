056cd731c64aa1074374c3f2d272aa9f0786d2b9
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-09-08 13:43:33 -0300

    Aula sobre asserções com o jest

---
+++ src/examples/test-jest-assertion.spec.ts
@@ -0,0 +1,38 @@
+describe('Primitive values', () => {
+  it('should test jest assertions', () => {
+    const number = 10;
+
+    expect(number).toBeGreaterThan(9);
+    expect(number).toBeGreaterThanOrEqual(10);
+    expect(number).toBeLessThan(11);
+    expect(number).toBeLessThanOrEqual(10);
+
+    expect(number).toBeCloseTo(10.001);
+    expect(number).toBeCloseTo(9.9996);
+
+    expect(number).toHaveProperty('toString');
+  });
+  it('should split test jest assertions', () => {
+    const number = 10;
+    expect(number).toBe(10);
+    expect(number).toEqual(10);
+
+    expect(number).not.toBeNull();
+    expect(number).not.toBeFalsy();
+    expect(number).toBeTruthy();
+  });
+});
+
+describe('Objects', () => {
+  it('should test jest assertions with objects', () => {
+    const person = { name: 'Carlos', age: 30 };
+    const anotherPerson = { ...person };
+
+    expect(person).toEqual(anotherPerson);
+    expect(person).toHaveProperty('age');
+    expect(person).not.toHaveProperty('lastName');
+    expect(person).toHaveProperty('age', 30);
+
+    expect(person.name).toBe('Carlos');
+  });
+});
