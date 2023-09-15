1b0750b6411d1ca1c658db6dd5f11470feac7134
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-09-15 13:12:29 -0300

    Aula testando o product

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,14 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number) => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+  it('should test properties', () => {
+    const sut = createSut('Camiseta', 49.9);
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+});
